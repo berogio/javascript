fcc31d130307884b39b5daf4f789aaada4b4d1b8
berogio <giorgi.beridze01e@geolab.edu.ge>
2022-05-11 17:21:49 +0200

    9

--- ubung.js
+++ ubung.js
@@ -1,6 +1,4 @@
 let wlovaneba = [`giorgi`, `gurami`, `tamo`, `mari`];
-if (wlovaneba.length == 4) {
-    console.log(`gamarjoba ${wlovaneba[3]}`);
-} else {
-    console.log(`arasworia`);
+for (let i = 0; i < wlovaneba.length; i++) {
+    console.log(`gamarjoba ${wlovaneba[i]}`);
 }
