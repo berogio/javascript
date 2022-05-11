53e27306ce42d47f16d6462689639fd6fe6e1c2b
berogio <giorgi.beridze01e@geolab.edu.ge>
2022-05-11 15:23:38 +0200

    update

--- ubung.js
+++ ubung.js
@@ -1,4 +1,15 @@
-const support = function() {
-    console.log(`გამარჯობა`)
+let wlovaneba = {
+    age: `giorgi`,
+    name: 15,
+    giorgi: [
+        15, 16, 17
+    ]
+
+};
+
+
+if (wlovaneba.age == `giorgi`) {
+    console.log(wlovaneba.giorgi[2]);
+} else {
+    console.log(`cudia`);
 }
-support();
